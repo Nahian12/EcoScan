ab3db72c6e795983dca8367cc632c3abc7ebf51b
Nahian12 <abidnahian2003@gmail.com>
2024-04-17 19:02:59 +0800

    navbar

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
-import {Button, Typography, Card, CardContent } from '@mui/material';
+//import {Button, Typography, Card, CardContent } from '@mui/material';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 import './App.css';
+import MyNavbar from './Components/Layout/MyNavbar';
 
 function App() {
   return (
-    <div >
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-
-      <div className="about-container">
-            <div className="about-content">
-                
-                <div className="about-p1">
-                    <Typography variant="h5" className="about-heading" style={{ marginTop: '10px' }}>
-                        HI
-                    </Typography>
-                </div>
-                <div className="card-container">
-                    {/* Card 1 */}
-                    <Card className="about-card" style={{ backgroundColor: '#27592d', width: 300, boxShadow: '5px 5px 5px 2px rgba(128, 128, 128, 0.5)' }}>
-                        {/* <CardHeader /> */}
-                    </Card>
-
-                    {/* Card 2 */}
-                    <Card className="about-card" style={{ backgroundColor: '#27592d', width: 300, boxShadow: '5px 5px 5px 2px rgba(128, 128, 128, 0.5)', }} >
-                    </Card>
-
-                    {/* Card 3 */}
-                    <Card className="about-card" style={{ backgroundColor: '#27592d', width: 300,  boxShadow: '5px 5px 5px 2px rgba(128, 128, 128, 0.5)' }}>
-                        {/* <CardHeader /> */}
-                        
-                    </Card>
-                </div>
-            </div>
-        </div>
+    <div>
+        <MyNavbar />
     </div>
 
     
---
+++ src/Components/Layout/MyNavbar.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import { Button, Navbar, Nav, Container, NavDropdown, Row } from 'react-bootstrap';
+
+
+
+export default class MyNavbar extends Component {
+  render() {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="dark">
+        <Container fluid>
+          <Navbar.Brand href="#" className="ms-2">
+            {/* <img
+              alt="EcoMap Logo"
+              src={EcoMapLogo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            /> */}
+            {" EcoScan"}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="ms-auto ms-2 my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link className="me-2" href="/about">About</Nav.Link>
+              <Nav.Link className="me-2" href="/cleanup">Cleanup</Nav.Link>
+              <Nav.Link className="me-2" href="/leaderboard">Leaderboards</Nav.Link>
+              <Nav.Link className="me-2" href="/global">Global Map</Nav.Link>
+              <Nav.Link className="me-2" href="/worldcup">World Cup</Nav.Link>
+              <Nav.Link className="me-2" href="/community">Community</Nav.Link>
+              <Nav.Link className="me-2" href="/Login">Login</Nav.Link>
+              <Nav.Link className="me-2" href="/signup">Signup</Nav.Link>
+              <Nav.Link className="me-2" href="/upload">Upload</Nav.Link>
+              <Nav.Link className="me-2" href="/tag">Tag Litter</Nav.Link>
+              <Nav.Link className="me-2" href="/gallery">Gallery</Nav.Link>
+              <Row className="d-flex flex-row align-items-center">
+                <NavDropdown align="end" title="More" className="me-2">
+                  <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
+                  <NavDropdown.Item href="/createTeam">Create Team</NavDropdown.Item>
+                  <NavDropdown.Item href="/joinTeam">Join Team</NavDropdown.Item>
+                  <NavDropdown.Item href="/myTeam">My Team</NavDropdown.Item>
+                  <NavDropdown.Item href="/settings">Settings</NavDropdown.Item>
+                  <NavDropdown.Item href="/language">Language</NavDropdown.Item>
+                  <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
+                </NavDropdown>
+              </Row>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+  }
+}
