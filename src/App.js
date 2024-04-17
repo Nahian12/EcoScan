4bcf117d9bbfb2dd7f07e55582a9d1b1750a7c67
Nahian12 <abidnahian2003@gmail.com>
2024-04-17 15:22:00 +0800

    MUI Trial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import {Button, Typography, Card, CardContent } from '@mui/material';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+
+      <div className="about-container">
+            <div className="about-content">
+                
+                <div className="about-p1">
+                    <Typography variant="h5" className="about-heading" style={{ marginTop: '10px' }}>
+                        HI
+                    </Typography>
+                </div>
+                <div className="card-container">
+                    {/* Card 1 */}
+                    <Card className="about-card" style={{ backgroundColor: '#27592d', width: 300, boxShadow: '5px 5px 5px 2px rgba(128, 128, 128, 0.5)' }}>
+                        {/* <CardHeader /> */}
+                    </Card>
+
+                    {/* Card 2 */}
+                    <Card className="about-card" style={{ backgroundColor: '#27592d', width: 300, boxShadow: '5px 5px 5px 2px rgba(128, 128, 128, 0.5)', }} >
+                    </Card>
+
+                    {/* Card 3 */}
+                    <Card className="about-card" style={{ backgroundColor: '#27592d', width: 300,  boxShadow: '5px 5px 5px 2px rgba(128, 128, 128, 0.5)' }}>
+                        {/* <CardHeader /> */}
+                        
+                    </Card>
+                </div>
+            </div>
+        </div>
     </div>
+
+    
   );
 }
 
